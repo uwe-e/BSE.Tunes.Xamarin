2075e069c7d43fb75dd28edb5e2591b5332c46fe
uwe-e <git.bsetunes@outlook.com>
2020-01-12 10:10:00 +0100

    cardview added

---
+++ BSE.Tunes.XApp/BSE.Tunes.XApp/Views/Templates/SlidePanelTemplate.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BSE.Tunes.XApp.Views.Templates
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SlidePanelTemplate : ContentView
+	{
+		public SlidePanelTemplate ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
